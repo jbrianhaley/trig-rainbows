d3fd7b17e6a66e030c59741c19e29e4fc99d84d5
Brian <jamesbrianhaley@Jamess-MacBook-Pro-8.local>
2020-01-25 20:59:51 -0500

    0.5 Added mouseover zoom and inset display of rgb, rgb freqs +/-= .01

--- colorPalette.js
+++ colorPalette.js
@@ -9,21 +9,52 @@ function changeColors(delta) {
   showPalette()
 }
 
+function focusColor(color) {
+  var factor = 2;
+  var colors = document.getElementById("colors").value;
+  var colorHeight = window.innerHeight / colors;
+  for (var i = 0; i < colors; i++) {
+    var name = "rectangle" + i;
+    if (i < color) {
+      document.getElementById(name).style.height = colorHeight-(colorHeight/colors) + "px";
+      document.getElementById(name).style.top = i*(colorHeight-(colorHeight/colors)) + "px";
+    } else if (i > color) {
+      document.getElementById(name).style.height = colorHeight-(colorHeight/colors) + "px";
+      document.getElementById(name).style.top = i*(colorHeight-(colorHeight/colors))+(colorHeight) + "px";
+    } else {
+      document.getElementById(name).style.height = colorHeight*factor + "px";
+      document.getElementById(name).style.top = i*(colorHeight-(colorHeight/colors)) + "px";
+      document.getElementById(name).style.fontSize = colorHeight*factor-8;
+      document.getElementById(name).style.width = 50 + 10*Math.sin((i*(360/colors))*Math.PI/180) + "%";
+      document.getElementById("summary").style.visibility = "hidden";
+      document.getElementById("colorDetail").style.visibility = "visible";
+      document.getElementById("colorDetail").style.backgroundColor = "rgb(" + document.getElementById(name).value + ")";
+      document.getElementById("colorDetail").innerHTML = "(" + document.getElementById(name).value + ")";
+      document.getElementById("colorDetail").style.fontSize = colorHeight*factor-8;
+      document.getElementById("colorDetail").style.verticalAlign = "middle";
+      document.getElementById("colorDetail").style.textAlign = "center";
+      document.getElementById("colorDetail").style.lineHeight = window.innerHeight*.35 + "px";
+      //document.getElementById("summary").value = document.getElementById(name).value;
+    }
+  }
+}
+
 function copyText() {
   var target = toString(document.getElementById("summary"));
   target.select();
   //target.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  document.getElementById("summary").value = "COPIED:&#13;" + document.getElementById("summary");
+  document.getElementById("summary").style.visibility = "visible"
+  document.getElementById("colorDetail").style.visibility = "hidden"
 }
 
 function changeFrequency(delta,color) {
   if (color == "r") {
-    document.getElementById("rfrequency").value = (parseFloat(document.getElementById("rfrequency").value) + parseFloat(delta)).toFixed(1);
+    document.getElementById("rfrequency").value = (parseFloat(document.getElementById("rfrequency").value) + parseFloat(delta)).toFixed(2);
   } else if (color == "g") {
-    document.getElementById("gfrequency").value = (parseFloat(document.getElementById("gfrequency").value) + parseFloat(delta)).toFixed(1);
+    document.getElementById("gfrequency").value = (parseFloat(document.getElementById("gfrequency").value) + parseFloat(delta)).toFixed(2);
   } else if (color == "b") {
-    document.getElementById("bfrequency").value = (parseFloat(document.getElementById("bfrequency").value) + parseFloat(delta)).toFixed(1);
+    document.getElementById("bfrequency").value = (parseFloat(document.getElementById("bfrequency").value) + parseFloat(delta)).toFixed(2);
   }
   showPalette();
 }
@@ -54,9 +85,9 @@ function resetPalette() {
 
 function randomPalette() {
   //document.getElementById("colors").value = Math.floor(getRandom(56))+8;
-  document.getElementById("rfrequency").value = getRandom(1).toFixed(1)*2;
-  document.getElementById("gfrequency").value = getRandom(1).toFixed(1)*2;
-  document.getElementById("bfrequency").value = getRandom(1).toFixed(1)*2;
+  document.getElementById("rfrequency").value = getRandom(1).toFixed(2)*2;
+  document.getElementById("gfrequency").value = getRandom(1).toFixed(2)*2;
+  document.getElementById("bfrequency").value = getRandom(1).toFixed(2)*2;
   showPalette()
   //document.getElementById("rdiff").value = Math.floor(getRandom(360));
   //document.getElementById("gdiff").value = Math.floor(getRandom(360));
@@ -82,7 +113,7 @@ function showPalette() {
   var colorHeight = h / colors;
   var paletteHTML = ("");  //init HTML for the display section
   for (var i = 0; i < colors; i++) {
-    paletteHTML += ("<div id=\x22rectangle" + i + "\x22 class=\x22colorRectangle\x22>rectangle " + i + "</div>\r");  //individual HTML for each color
+    paletteHTML += ("<div id=\x22rectangle" + i + "\x22 class=\x22colorRectangle\x22 onmouseover=\x22focusColor(" + i + ")\x22 onmouseout=\x22showPalette()\x22>rectangle " + i + "</div>\r");  //individual HTML for each color
   }
   document.getElementById("content").innerHTML = paletteHTML;
   for (var i = 0; i < colors; i++) {
@@ -91,6 +122,7 @@ function showPalette() {
     var b = parseInt(Math.sin(bfrequency*i + (bDiff*Math.PI/180)) * colorAmplitude + colorCenter);
     paletteText += ("rgb(" + r + "," + g + "," + b + ")\r");
     var name = "rectangle" + i;
+    document.getElementById(name).value = [r,g,b];
     document.getElementById(name).innerHTML = "color "+ i +": rgb(" + r + "," + g + "," + b + ")";
     document.getElementById(name).style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     document.getElementById(name).style.top = i*colorHeight + "px";
@@ -98,5 +130,7 @@ function showPalette() {
     document.getElementById(name).style.height = colorHeight-1 + "px";
     document.getElementById(name).style.fontSize = colorHeight*3/4;
   }
+  document.getElementById("summary").style.visibility = "visible";
+  document.getElementById("colorDetail").style.visibility = "hidden";
   document.getElementById("summary").value = paletteText;
 }
